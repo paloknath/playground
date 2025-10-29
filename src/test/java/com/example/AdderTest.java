6e7e54628ee021d3ef1e66b6ae47b58c589c5b97
alok <p.aloknath@gmail.com>
2025-10-28 22:10:03 -0400

    SCRUM-5: Add new class to add two numbers
    Add new class to add two numbers

---
+++ src/test/java/com/example/AdderTest.java
@@ -0,0 +1,31 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AdderTest {
+
+    @Test
+    void testAddPositiveNumbers() {
+        Adder adder = new Adder();
+        assertEquals(5, adder.add(2, 3));
+    }
+
+    @Test
+    void testAddNegativeNumbers() {
+        Adder adder = new Adder();
+        assertEquals(-5, adder.add(-2, -3));
+    }
+
+    @Test
+    void testAddPositiveAndNegativeNumbers() {
+        Adder adder = new Adder();
+        assertEquals(1, adder.add(3, -2));
+    }
+
+    @Test
+    void testAddZero() {
+        Adder adder = new Adder();
+        assertEquals(0, adder.add(0, 0));
+    }
+}
